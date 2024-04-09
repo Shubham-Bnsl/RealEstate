e7ccbec94e8518e32d93b34ca90ed0361b36b6de
Shubham Bansal <20eo1057@mitsgwl.ac.in>
2024-04-10 01:07:46 +0530

    pages and routes created

--- real-Estate/src/App.jsx
+++ real-Estate/src/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignOut from './pages/SignOut';
+
 
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Home /> ,
+    },
+    {
+      path: "/about",
+      element:<About /> ,
+    },
+    {
+      path: "/profile",
+      element:<Profile /> ,
+    },
+    {
+      path: "/signin",
+      element:<Signin /> ,
+    },
+    {
+      path: "/signOut",
+      element:<SignOut /> ,
+    },
+  ]);
+
   return (
     <div>
-      <h1>HEllo</h1>
+     <RouterProvider router={router} />
     </div>
   )
 }
