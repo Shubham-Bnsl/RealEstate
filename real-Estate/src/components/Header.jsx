bae79e81148ae60dbc0aae9090318186edc2df43
Shubham Bansal <20eo1057@mitsgwl.ac.in>
2024-04-10 18:38:17 +0530

    created and run the server

--- real-Estate/src/components/Header.jsx
+++ real-Estate/src/components/Header.jsx
@@ -17,13 +17,13 @@ const Header = () => {
             <FaSearch className='text-slate-600' />
         </form>
         <ul className='flex gap-4'>
-            <Link to={'/'} >
+            <Link to='/'>
             <li className='hidden sm:inline text-slate-700 hover:underline '>Home</li>
             </Link>
-            <Link to={'/about'}>
+            <Link to='/about'>
             <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
-            <Link to={'/sign-in'}>
+            <Link to='/sign-in'>
             <li className=' text-slate-700 hover:underline'>Sign in</li>
             </Link>
         </ul>
